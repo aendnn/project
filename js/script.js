f3e3a9a591cf66603ce745b0dd263124130d2b6e
Artyem <frostball3@gmail.com>
2023-06-14 00:17:41 +0300

    123

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 customSelect('select');
 
 Ф
+// slider
+const toggles = document.querySelectorAll('.slider-button');
+const slides = document.querySelectorAll('.big-offers-slide');
+const wrapper = document.querySelector('.wrapper');
+
+toggles.forEach((item, Index) => {
+  item.addEventListener('click' , () => {
+  slides.forEach(item => item.classList.remove('current-slide'));
+  toggles.forEach(toggle => toggle.parentElement.classList.remove('current-slider-item'));
+
+  item.parentElement.classList.add('current-slider-item');
+  slides[index].classList.add('current-slide');
+  if (index === 0) {
+    wrapper.style.backgroundColor = "pink";
+  }
+  else if (index === 1) {
+    wrapper.style.backgroundColor = "blue";
+  }
+  else {
+    wrapper.style.backgroundColor = "green";
+  }
+});
